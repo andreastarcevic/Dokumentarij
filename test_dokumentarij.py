994c1840b0013687c589735ee0eaf1ded0b53e43
andreastarcevic <64777442+andreastarcevic@users.noreply.github.com>
2020-06-01 14:26:43 +0200

    Update test_dokumentarij.py

--- test_dokumentarij.py
+++ test_dokumentarij.py
@@ -1,16 +1,16 @@
 import unittest
-from dokumentarij import Dokumentarij
+from dokumentarij import DokumentarijModel, DokumentarijView, DokumentarijController
 
 class DokumentarijTestCase(unittest.TestCase):
 
     def test_if_someone_in_shownames(self):
-        game_test = Dokumentarij()
-        names = game_test.show_names()
-        self.assertEqual(str(names), "U bazi se nalaze imena studenata")
+        game_test = DokumentarijController(DokumentarijModel(), DokumentarijView())
+        number_of_students = len(names)
+        self.assertGreater(number_of_students, 0, "U bazi imena studenata nije nitko na popisu")
 
     def test_getnewname(self):
-        game_test = Dokumentarij()
-        new_name = game_test.create_names()
+        game_test = DokumentarijController(DokumentarijModel(), DokumentarijView())
+        new_name = game_test.view.create_names()
         self.assertEqual(str(new_name), "Dodaju se imena studenata u listu")
 
 
